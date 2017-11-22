56f387652788ada0e13308273d99e0e4f8c461d1
Amanuel Wolde-Kidan <AmanuelWK@hiraiwamac.physik.fu-berlin.de>
2017-11-22 13:39:54 +0100

    before I falsely set dt_1 = 2 min instead of 3 min, changed now

--- DiffusionEq_Block_reflective.py
+++ DiffusionEq_Block_reflective.py
@@ -297,14 +297,8 @@ def main():
     # dxx_dist has one element more than dxx_width because it for WMatrix
     # computation dx at i+1 is necccessary --> needed for last bin too
 
-    # NOTE: building c0 profile, assume c0 const. in bulk (for x < 50 µm)
+    # NOTE: building c0 profile, extrapolated and assume c0 const. in bulk
     c_const = cc[0, -1]  # take first value of last profile as c0
-    # gel = 14  # number of bins for gel
-    # bulk = dxx_width.size - gel  # number of bins for bulk
-    # c0 = np.concatenate((np.ones(bulk)*c_const, np.zeros(gel)))
-    # cc = [cc[:, i] for i in range(cc[0, :].size)]
-    # cc = [c0] + cc  # add c0 profile to list of all profiles
-    # NOTE: now taking c0 profile from extrapolation
     # overriding c0 profile with extrapolated values for last three bins
     c0 = np.array([20.43270674, 20.39872678, 20.20967943, 19.82748853,
                    19.21407792, 18.33137147, 17.14129302, 15.6057664,
@@ -313,8 +307,6 @@ def main():
                    3.06770597])
     c0 = np.concatenate((np.ones(6)*c_const, c0))
     cc = [c0] + [cc[:, i] for i in range(1, cc[0, :].size)]  # now with c0
-    t0 = 120  # time after c0 profile
-    tt = np.concatenate((np.zeros(1), tt+t0)).astype(int)
 
     # overriding bounds for custom set of parameters
     DBound = 1000
